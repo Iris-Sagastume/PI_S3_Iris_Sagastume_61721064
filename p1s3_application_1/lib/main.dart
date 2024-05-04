b3b4a8b28a128ce220daa9042d0fd8c207fe905d
Iris Vanessa Sagastume Nataren <167151929+Iris-Sagastume@users.noreply.github.com>
2024-05-03 20:15:23 -0600

    ejemplo statefull

--- p1s3_application_1/lib/main.dart
+++ p1s3_application_1/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class ColorChangeWidget extends StatefulWidget {
@@ -8,26 +9,36 @@ class ColorChangeWidget extends StatefulWidget {
 class _ColorChangeWidgetState extends State<ColorChangeWidget> {
   //estado _currentColor representa el color actual del contenedor.
   Color _currentColor = Colors.blue;
-//Cuando el usuario toca el contenedor, se llama a la función _toggleColor(), 
-//que cambia _currentColor entre azul y rojo alternativamente
-  void _toggleColor() {
+//Cuando el usuario toca el contenedor, se llama a la función _changeColor(),
+//que genera un nuevo color aleatorio utilizando _generateRandomColor() y actualiza _currentColor.
+  Color _generateRandomColor() {
+    Random random = Random();
+    return Color.fromRGBO(
+      random.nextInt(256), // Red
+      random.nextInt(256), // Green
+      random.nextInt(256), // Blue
+      1.0, // Alpha (opacidad)
+    );
+  }
+
+  void _changeColor() {
     setState(() {
-      _currentColor = _currentColor == Colors.blue ? Colors.red : Colors.blue;
+      _currentColor = _generateRandomColor();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-//GestureDetector para detectar el toque del usuario en el contenedor y llamar a la función _toggleColor()
+//Se utiliza un GestureDetector para detectar el toque del usuario en el contenedor y llamar a la función _changeColor().
     return GestureDetector(
-      onTap: _toggleColor,
+      onTap: _changeColor,
       child: Container(
-        width: 200,
+        width: 100,
         height: 100,
         color: _currentColor,
         child: Center(
           child: Text(
-            'Preciona el botón',
+            'Tap me',
             style: TextStyle(
               color: Colors.white,
               fontWeight: FontWeight.bold,
@@ -44,7 +55,7 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text('Prueba Intermedia de Programación Móvil'),
+        title: Text('Prueba Intermedia'),
       ),
       body: Center(
         child: ColorChangeWidget(),
